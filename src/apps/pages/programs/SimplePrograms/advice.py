eb4ec527feb65c9ae1376700a28f64cc6bef0471
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-05-21 14:47:22 +0530

    add reload btn in advice

--- src/apps/pages/programs/SimplePrograms/advice.py
+++ src/apps/pages/programs/SimplePrograms/advice.py
@@ -5,3 +5,6 @@ def advice():
   res = requests.get("https://api.adviceslip.com/advice").json()
   advice_text = res['slip']['advice']
   st.markdown(f"#### 💡 **{advice_text}**")
+
+  if st.button("🔄 Reload Advice"):
+    st.session_state['reload_advice'] = not st.session_state.get('reload_advice', False)
