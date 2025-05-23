9c206a89ca2d10120428496d505aef6ab5588180
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-05-23 16:43:21 +0530

    fixes layout in music api prog

--- src/apps/pages/programs/ApiPrograms/music.py
+++ src/apps/pages/programs/ApiPrograms/music.py
@@ -1,7 +1,7 @@
-import streamlit as st
-import os
-import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import streamlit as st
+import spotipy
+import os
 
 from src.helpers.displayInstructions import showInstructions
 from src.helpers.checkKeyExist import isKeyExist
@@ -25,42 +25,48 @@ def authenticateSpotify():
     return sp
   return None
 
-def fetchMusicData(sp, search_query, search_type):
+def fetchMusicData(sp, search_query, search_type, limits):
   if search_type == 'track':
-    results = sp.search(q=search_query, type='track', limit=10)
+    results = sp.search(q=search_query, type='track', limit=limits)
     return results['tracks']['items']
   elif search_type == 'artist':
-    results = sp.search(q=search_query, type='artist', limit=10)
+    results = sp.search(q=search_query, type='artist', limit=limits)
     return results['artists']['items']
   elif search_type == 'album':
-    results = sp.search(q=search_query, type='album', limit=10)
+    results = sp.search(q=search_query, type='album', limit=limits)
     return results['albums']['items']
 
 def displayResults(results, search_type):
   if search_type == 'track':
     for track in results:
-      st.image(track["album"]["images"][0]["url"], caption=track["name"], use_column_width=True)
-      st.write(f"Artist: {track['artists'][0]['name']}")
-      st.write(f"Album: {track['album']['name']}")
-      st.write(f"Release Date: {track['album']['release_date']}")
-      if track['preview_url']:
-        st.write("Preview:")
-        st.audio(track['preview_url'])
-      else:
-        st.write("Preview not available.")
-      st.divider()
-  elif search_type == 'artist':
-    for artist in results:
-      if artist["images"]:
-        st.image(artist["images"][0]["url"], caption=artist["name"], use_column_width=True)
-      st.write(f"Followers: {artist['followers']['total']}")
-      st.write(f"Genres: {', '.join(artist['genres'])}")
+      col1, col2 = st.columns(2)
+      with col1:
+        st.image(track["album"]["images"][0]["url"], caption=track["name"])
+      with col2:
+        with st.expander("Track Details", expanded=True):
+          st.markdown(f"##### Artist: {track['artists'][0]['name']}")
+          st.markdown(f"##### Album: {track['album']['name']}")
+          st.markdown(f"##### Release Date: {track['album']['release_date']}")
       st.divider()
   elif search_type == 'album':
     for album in results:
-      st.image(album["images"][0]["url"], caption=album["name"], use_column_width=True)
-      st.write(f"Artist: {album['artists'][0]['name']}")
-      st.write(f"Release Date: {album['release_date']}")
+      col1, col2 = st.columns(2)
+      with col1:
+        st.image(album["images"][0]["url"], caption=album["name"])
+      with col2:
+        with st.expander("Album Details", expanded=True):
+          st.markdown(f"##### Artist: {album['artists'][0]['name']}")
+          st.markdown(f"##### Release Date: {album['release_date']}")
+      st.divider()
+  elif search_type == 'artist':
+    for artist in results:
+      col1, col2 = st.columns(2)
+      with col1:
+        st.image(artist["images"][0]["url"], caption=artist["name"])
+      with col2:
+        with st.expander("Artist Details", expanded=True):
+          st.markdown(f"##### Followers: {artist['followers']['total']}")
+          st.markdown(f"##### Genres: {', '.join(artist['genres'])}")
       st.divider()
 
 def music():
@@ -76,10 +82,11 @@ def music():
 
   col1, col2 = st.columns(2)
   with col1:
-    search_query = st.text_input("Enter artist, album, or track name")
+    search_query = st.text_input("Enter artist, album, or track name", placeholder="Type here...")
   with col2:
     search_type = st.selectbox("Select search type", ["track", "album", "artist"])
+  limits = st.slider("Select number of results", min_value=1, max_value=50, value=10)
   if st.button("Search") and search_query:
-    with st.spinner("Fetching results..."):
-      results = fetchMusicData(sp, search_query, search_type)
+    with st.spinner("Getting results..."):
+      results = fetchMusicData(sp, search_query, search_type, limits)
       displayResults(results, search_type)
